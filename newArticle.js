d6739cd09626bd9e33d0a91e1ea39e1d9c76af09
seeen <inferrinizzard@gmail.com>
2018-07-01 13:32:07 -0700

    article test

---
+++ newArticle.js
@@ -0,0 +1,13 @@
+function appendPost(type) {
+    var parent = document.getElementById(type);
+    var article = document.createElement('article');
+    parent.appendChild(article);
+    var link = document.createElement('a');
+    article.appendChild(link);
+    var title = document.createElement('h2')
+    var line = document.createElement('hr');
+    var media = document.createElement('embed');
+    link.appendChild(title);
+    link.appendChild(line);
+    link.appendChild(media);
+}
