d1fd531f09f250e26e0f4d1d8a987fb6f827cdbc
seeen <inferrinizzard@gmail.com>
2018-07-04 10:11:34 -0700

    article spawn test

--- newArticle.js
+++ newArticle.js
@@ -1,15 +1,47 @@
 function appendPost(type) {
-    var parent = document.getElementById(type);
+    var id = toString(type);
+
+    var parent = document.getElementById(id);
     var article = document.createElement('article');
     parent.appendChild(article);
+
     var link = document.createElement('a');
+    var linkAnchor = "";
+    link.setAttribute('href',linkAnchor);
     article.appendChild(link);
-    var title = document.createElement('h2')
+
+    var img = document.createElement('img');
+    var imgFile = Image;
+    img.setAttribute('src',imgFile);
+    link.appendChild(img);
+
+    var section = document.createElement('section');
+    link.appendChild(section);
+
+    var vignette = document.createElement('div');
+    vignette.setAttribute('id',"vignette");
+    section.appendChild(vignette);
+
+    var title = document.createElement('span');
+    var titleText = "";
+    var titleNode = document.createTextNode(titleText + " - ");
+    title.appendChild(titleNode);
+    vignette.appendChild(title);
+
+    var date = document.createElement('span'); 
+    date.setAttribute('class', "date");
+    var dateText = "";
+    var dateNode = document.createTextNode(dateText);
+    date.appendChild(dateNode);
+    vignette.appendChild(date);
+
     var line = document.createElement('hr');
-    var media = document.createElement('embed');
-    link.appendChild(title);
-    link.appendChild(line);
-    link.appendChild(media);
-    title.innerHTML = test;
-    
+    vignette.appendChild(hr);
+
+    var caption = document.createElement('h4');
+    var captText = "";
+    var captNode = document.createTextNode(captText);
+    caption.appendChild(captText);
+    vignette.appendChild(caption);  
+
 }
