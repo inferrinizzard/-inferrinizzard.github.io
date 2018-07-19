763299e3f7c936331e949b5b8d9bfa5883de265d
seeen <inferrinizzard@gmail.com>
2018-07-19 14:22:09 -0700

    new jquery integrity sha

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,7 @@
 $(function(){
     var type = $(".count").attr("id");
-    $(".col-md-4 #anchor").load("/test.html ." + type + ":eq(0)");
-    $(".col-md-4 #anchor").load("/test.html ." + type + ":eq(1)");
-    $(".col-md-4 #anchor").load("/test.html ." + type + ":eq(2)");
+    var anchor = $(".col-md-4 #anchor");
+    anchor.load("/test.html ." + type + ":eq(0)");
+    anchor.load("/test.html ." + type + ":eq(1)");
+    anchor.load("/test.html ." + type + ":eq(2)");
 })
