7904d59acf2fe3ba17210ea07e02ce016d271e48
seeen <inferrinizzard@gmail.com>
2018-07-19 18:53:50 -0700

    columns fix test

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,6 @@
 $(function(){
     var type = $(".count").attr("id");
-    var anchor = $(".col-md-4 #anchor");
+    var anchor = $(".side-bar #anchor");
     anchor.load("/test.html ." + type + ":eq(0)");
     // anchor.load("/test.html ." + type + ":eq(1)");
     // anchor.load("/test.html ." + type + ":eq(2)");
