a9613bcee6fc0e4f132fd0d34f4b5e99080c9682
seeen <inferrinizzard@gmail.com>
2018-07-20 16:43:22 -0700

    sidedar test 8

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,7 @@
 $(function(){
     var type = String($(".count").attr("id"));
-    alert(type);
-    var anchor = $(".side-bar #anchor");
+    var anchor = $("#anchor");
     anchor.load("/test.html ." + type + ":eq(0)");
     anchor.load("/test.html ." + type + ":eq(1)");
     anchor.load("/test.html ." + type + ":eq(2)");
-    alert(anchor);
 })
