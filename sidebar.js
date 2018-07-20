86c32cb7c8e10a13f58817ab4b78c637b42431a8
seeen <inferrinizzard@gmail.com>
2018-07-20 16:34:55 -0700

    sidebar test 7

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,9 @@
 $(function(){
-    var type = $(".count").attr("id");
+    var type = String($(".count").attr("id"));
+    alert(type);
     var anchor = $(".side-bar #anchor");
     anchor.load("/test.html ." + type + ":eq(0)");
     anchor.load("/test.html ." + type + ":eq(1)");
     anchor.load("/test.html ." + type + ":eq(2)");
-    $(".side-bar #anchor").load("/test.html .unity:eq(0)");
+    alert(anchor);
 })
