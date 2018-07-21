bd46ee58dd7d2ababfb430ebf1e39f09b9455736
seeen <inferrinizzard@gmail.com>
2018-07-20 22:12:11 -0700

    sidebar 9

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,7 @@
 $(function(){
     var type = String($(".count").attr("id"));
     var anchor = $("#anchor");
-    anchor.load("/test.html ." + type + ":eq(0)");
-    anchor.load("/test.html ." + type + ":eq(1)");
-    anchor.load("/test.html ." + type + ":eq(2)");
+    anchor.append($("<div>").load("/test.html ." + type + ":eq(0)").html());
+    // anchor.load("/test.html ." + type + ":eq(1)");
+    // anchor.load("/test.html ." + type + ":eq(2)");
 })
