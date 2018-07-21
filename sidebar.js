c1c1ac612f06627e243f703cf99587e4e556e0bd
seeen <inferrinizzard@gmail.com>
2018-07-21 13:31:56 -0700

    sdibear 14

--- sidebar.js
+++ sidebar.js
@@ -3,7 +3,7 @@ $(function(){
     var anchor = $("#anchor");
     var temp = $("<div></div>");
     temp.load("/test.html ." + type + ":eq(0)").html();
-    temp = temp.html();
+    // temp = temp.html();
     anchor.append(temp);
     // anchor.load("/test.html ." + type + ":eq(1)");
     // anchor.load("/test.html ." + type + ":eq(2)");
