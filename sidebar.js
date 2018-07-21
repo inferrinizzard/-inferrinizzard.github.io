67cf2ff3e0a0a640b21e382816507cabeab80f8f
seeen <inferrinizzard@gmail.com>
2018-07-21 13:51:26 -0700

    aidebar 16

--- sidebar.js
+++ sidebar.js
@@ -5,8 +5,10 @@ $(function(){
     temp.load("/test.html ." + type + ":eq(0)").html();
     // temp = temp.html();
     anchor.append(temp);
+    temp = $("<div></div>");
     temp.load("/test.html ." + type + ":eq(1)").html();
     anchor.append(temp);
+    temp = $("<div></div>");
     temp.load("/test.html ." + type + ":eq(2)").html();
     anchor.append(temp);
 })
