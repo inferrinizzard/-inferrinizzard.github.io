f400a37b265609e8978957c319113dcae2bd6e01
seeen <inferrinizzard@gmail.com>
2018-07-21 11:56:34 -0700

    sdiebar 11

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,10 @@
 $(function(){
     var type = String($(".count").attr("id"));
     var anchor = $("#anchor");
-    alert(load("/test.html ." + type + ":eq(0)").html());
-    anchor.append(load("/test.html ." + type + ":eq(0)").html());
+    var temp = $("<div></div>");
+    temp.load("/test.html ." + type + ":eq(0)").html();
+    alert(temp);
+    anchor.append(temp);
     // anchor.load("/test.html ." + type + ":eq(1)");
     // anchor.load("/test.html ." + type + ":eq(2)");
 })
