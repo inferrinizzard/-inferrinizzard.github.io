3af1044fb60f04b01cbb71332b70e4a2f2bb9903
seeen <inferrinizzard@gmail.com>
2018-07-20 22:19:50 -0700

    aidebar 10

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,8 @@
 $(function(){
     var type = String($(".count").attr("id"));
     var anchor = $("#anchor");
-    anchor.append($("<div>").load("/test.html ." + type + ":eq(0)").html());
+    alert(load("/test.html ." + type + ":eq(0)").html());
+    anchor.append(load("/test.html ." + type + ":eq(0)").html());
     // anchor.load("/test.html ." + type + ":eq(1)");
     // anchor.load("/test.html ." + type + ":eq(2)");
 })
