ee0191750f28259afc1b9f3bfc4b290a37eaac59
seeen <inferrinizzard@gmail.com>
2018-07-24 16:17:17 -0700

    new otf, sidbear 17

--- sidebar.js
+++ sidebar.js
@@ -2,13 +2,16 @@ $(function(){
     var type = String($(".count").attr("id"));
     var anchor = $("#anchor");
     var temp = $("<div></div>");
-    temp.load("/index.html ." + type + ":eq(0)").html();
+    // temp.load("/index.html ." + type + ":eq(0)").html();
+    temp.load("/test.html ." + type + ":eq(0)").html();
     // temp = temp.html();
     anchor.append(temp);
     temp = $("<div></div>");
-    temp.load("/index.html ." + type + ":eq(1)").html();
+    // temp.load("/index.html ." + type + ":eq(1)").html();
+    temp.load("/test.html ." + type + ":eq(1)").html();
     anchor.append(temp);
     temp = $("<div></div>");
-    temp.load("/index.html ." + type + ":eq(2)").html();
+    // temp.load("/index.html ." + type + ":eq(2)").html();
+    temp.load("/test.html ." + type + ":eq(2)").html();
     anchor.append(temp);
 })
